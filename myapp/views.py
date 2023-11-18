1c28627fc727d5bab681285a556a69a7535eb8f1
ondi <omachacek@gmail.com>
2023-11-18 13:41:17 +0100

    working model

--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     # return HttpResponse("hello world!")
     return render(request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", { "todos": items})
